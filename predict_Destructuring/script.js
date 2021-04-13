66fd92ab7ac4799de849e59e918c31b23d87af40
Stefano <piero.quiroz93@gmail.com>
2021-04-13 18:56:32 -0500

    Problem Number Two

--- predict_Destructuring/script.js
+++ predict_Destructuring/script.js
@@ -1,7 +1,21 @@
 //Problema # 1
+/*
 const cars = ['Tesla', 'Mercedes', 'Honda']
 const [ randomCar ] = cars
 const [ ,otherRandomCar ] = cars
 //Predict the output
 console.log(randomCar) // Tesla
 console.log(otherRandomCar) // Mercedes
+
+//Problema 2
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name); //name is not defined
+console.log(otherName); //Elon
+*/
+//Problema 3 
