293a0bdb251d1d034312f29bdd60ff3bfdad641a
Stefano <piero.quiroz93@gmail.com>
2021-04-13 19:05:30 -0500

    Problem Finnish

--- predict_Destructuring/script.js
+++ predict_Destructuring/script.js
@@ -39,3 +39,16 @@ console.log(first, second, third); // 2 5 2
 console.log(first == second); //2 == 5 false
 console.log(first == third); // 2 == 2 true
 */
+//Problema 5
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key); //value
+console.log(secondKey); // [1,5,1,8,3,3]
+console.log(secondKey[0]); // 1
+console.log(willThisWork); // 5
