273b82c423d159f91a18bf11c8b9476d4aafbd55
Stefano <piero.quiroz93@gmail.com>
2021-04-13 19:02:28 -0500

    Problem Number Four

--- predict_Destructuring/script.js
+++ predict_Destructuring/script.js
@@ -28,5 +28,14 @@ const { password: hashedPassword } = person;
 //Predict the output
 console.log(password); //12345
 console.log(hashedPassword);//undefined
-*/
+
 //Problema 4
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers; //2
+const [,,,second] = numbers; //5
+const [,,,,,,,,third] = numbers; //2
+//Predict the output
+console.log(first, second, third); // 2 5 2
+console.log(first == second); //2 == 5 false
+console.log(first == third); // 2 == 2 true
+*/
