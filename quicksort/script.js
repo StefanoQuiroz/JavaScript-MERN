1d912f0d56c520f192a78fbc0df5e40893382081
Stefano <piero.quiroz93@gmail.com>
2021-04-14 18:32:12 -0500

    Pre Partition Hoar

---
+++ quicksort/script.js
@@ -0,0 +1,14 @@
+const partition = (arr, left, right) =>{
+    let pivot = Math.floor((left+right)/2);
+    let izq=[], der=[];
+    for(let i=left; i<pivot; i++){
+        izq.push(arr[i]);        
+    }
+    for(let j=right; j>pivot;j--){
+        der.push(arr[j]);        
+    }
+    return [izq, arr[pivot], der];
+}
+let array = [1,2,3,4,5,6,7,8]
+//1,2,3,  4  , 5,6,7,8
+console.log(partition(array, 0, array.length-1));
