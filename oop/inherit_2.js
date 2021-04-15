5c8cc7f6058d733d0e4dd079b491a467629d688b
Stefano <piero.quiroz93@gmail.com>
2021-04-15 17:01:06 -0500

    Inherit class V2 finished

--- oop/inherit_2.js
+++ oop/inherit_2.js
@@ -10,7 +10,7 @@ class Vehicle {
         this.miles += 10;
         //curiosity this.constructor.name => nameClass, great!!
         console.log(`You drove your ${this.constructor.name} and it now has ${this.miles} miles on it`);
-        console.log(`You drove your ${this.manufacturer} and it now has ${this.miles} miles on it`);
+        console.log(`You drove your ${this.manufacturer} ${this.model} ${this.color} and it now has ${this.miles} miles on it`);
     }
     parentFunction(){
         return "This is coming from the parent!";
@@ -18,21 +18,21 @@ class Vehicle {
 }
 //Implementig class M5
 class M5 extends Vehicle {
-    constructor(color){
+    constructor(color, hp){
         super("BMW", "M5", color);
         //calling Vehicle´s constructor
+        this.hp = hp;
     }
     childFunction(){
         //using super, we can call the method parentFunction() class Vehicle{ }
-        const message = super.parentFunction();
-        console.log(message);
+        // const message = super.parentFunction();
+        const message = super.drive();  
+        console.log(`${message}`);
     }
 }
 
-const m5 = new M5("Blue");
+const m5 = new M5("Blue", 450);
 m5.childFunction();
-
-
 
 // const car = new Vehicle("Chevrolet", "Capricce", "brown");
 // car.drive();
