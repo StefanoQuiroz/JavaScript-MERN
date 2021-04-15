e3b97159aca5c5b7693e7127e08837b77946ef1f
Stefano <piero.quiroz93@gmail.com>
2021-04-15 16:54:47 -0500

    Add class child - Inherit

--- oop/inherit_2.js
+++ oop/inherit_2.js
@@ -12,7 +12,28 @@ class Vehicle {
         console.log(`You drove your ${this.constructor.name} and it now has ${this.miles} miles on it`);
         console.log(`You drove your ${this.manufacturer} and it now has ${this.miles} miles on it`);
     }
+    parentFunction(){
+        return "This is coming from the parent!";
+    }
+}
+//Implementig class M5
+class M5 extends Vehicle {
+    constructor(color){
+        super("BMW", "M5", color);
+        //calling Vehicle´s constructor
+    }
+    childFunction(){
+        //using super, we can call the method parentFunction() class Vehicle{ }
+        const message = super.parentFunction();
+        console.log(message);
+    }
 }
 
-const car = new Vehicle("Chevrolet", "Capricce", "brown");
-car.drive();
+const m5 = new M5("Blue");
+m5.childFunction();
+
+
+
+// const car = new Vehicle("Chevrolet", "Capricce", "brown");
+// car.drive();
+// console.log(car.parentFunction());
