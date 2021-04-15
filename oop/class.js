44c2cd27bff5e02b8bd6b334a86303fcf0611761
Stefano <piero.quiroz93@gmail.com>
2021-04-15 16:00:59 -0500

    Create new instance from Vehicle class and using console.table(object)

--- oop/class.js
+++ oop/class.js
@@ -18,3 +18,10 @@ class Vehicle{
 }
 
 //To create a new instance we must provide the constructor with the following arguments   
+//create a new object car invoke new class Vehicle with their arguments
+const car = new Vehicle("Ford", "f-150", "black");
+console.log(car.miles);//0
+car.drive(); //+=10
+//print newobject.property ---> ex: car.manufacturer
+console.table(`${car.miles} ${car.manufacturer} ${car.model} ${car.color}`);
+console.table(car);//Using console.table for index - value in arrays, objects (objects)
