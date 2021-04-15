bfeebaefcb3197421bdd9868f25ac8388ed8e055
Stefano <piero.quiroz93@gmail.com>
2021-04-14 20:44:32 -0500

    Partition finished

--- quicksort/script.js
+++ quicksort/script.js
@@ -1,15 +1,38 @@
-//Hoare partition
-const partition = (arr, left, right) =>{
-    let pivot = Math.floor((left+right)/2);
-    let izq=[], der=[];
-    for(let i=left; i<pivot; i++){
-        izq.push(arr[i]);        
+//algorithm Hoare
+//call function
+//partition(array, left, right)
+//return pivote-index
+
+//function quickSort() calling partion function() to sorting
+//quick sorting use the pivote-index
+
+//donde sera la llamada recursiva
+//cuando sabemos que ina arte de la matriz ha aisdo ordenada
+//cuándo sabemos que se ha ordenado toda la matriz
+
+const partitionHoare = (array, left, right) => {
+    let pivot = Math.floor((left + right) / 2);
+    while (left < right) {
+        while (array[left] < array[pivot]) {
+            left++
+        }
+        while (array[right] > array[pivot]) {
+            right--
+        }
+        if (left <= right) {
+            let temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            left++
+            right--
+        }
     }
-    for(let j=right; j>pivot;j--){
-        der.push(arr[j]);        
-    }
-    return [izq, arr[pivot], der];
+    return left;
 }
-let array = [1,2,3,4,5,6,7,8,9]
-//1,2,3,  4  , 5,6,7,8
-console.log(partition(array, 0, array.length-1));
+// i=5 j=1
+// i=1 j=5
+// i=4 j=2
+//i =2 j =4
+// i, j=2 
+let arr = [5,4,2,1,3];
+console.log(partitionHoare(arr, 0, arr.length-1));
