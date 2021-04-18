33f06ba29cf6fb0abbabdaeef8e08b7a4956ba8f
Stefano <piero.quiroz93@gmail.com>
2021-04-18 16:21:05 -0500

    gameCards finished and modiefied

--- gameCards/gameCards.js
+++ gameCards/gameCards.js
@@ -3,7 +3,6 @@ class Carta {
         this.nombre=nombre;
         this.costo=costo;
     }
-    //methods
 }
 
 class Unidad extends Carta{
@@ -11,10 +10,16 @@ class Unidad extends Carta{
         super(nombre, costo);
         this.poder = poder;
         this.resiliencia = resiliencia;
+        this.efectos=[];
+        this.asesino = null;
     }
     attack(target){
         if(!(target instanceof Unidad)) throw new Error('Solo puedes atacar a otra unidad'); 
         target.resiliencia -= this.poder;
+        target.asesino=this.nombre;
+        if(target.resiliencia <= 0){
+            console.log(`${target.nombre} fue asesinado por ${target.asesino}`)
+        }
     }
 }
 
@@ -26,30 +31,35 @@ class Efectos extends Carta{
         this.magnitud = magnitud;
     }
     played(target){
-        if(!(target instanceof Unidad)) throw new Error('Solo puedes atacar a otra unidad');
-        target[this.stat] += this.magnitud;
-    }
-      
-        
+        if(!(target instanceof Unidad)){
+            throw new Error('Solo puedes atacar a otra unidad')
+        } else {
+            //incrementar o reducir su magnitud
+            target[this.stat] += this.magnitud;
+            //añadiendo un nuevo efecto
+            target.efectos.push(this.stat);
+        }
+    }      
 }
 
-const redBelt = new Unidad("Ninja Cinturon Rojo", 2, 3, 3);
-const blackBelt = new Unidad ("Ninja Cinturon Negro",2,3,3);
-
+const redBelt = new Unidad("Ninja Cinturon Rojo", 3, 3, 4);
+const blackBelt = new Unidad ("Ninja Cinturon Negro",4,5,4);
+//target[this.stat = "resiliencia" accede a valor del key resiliencia]
 const algoritmoDíficil = new Efectos('Algoritmo Difícil', 2, 'aumentar resistencia del objetivo en 3', 'resiliencia', 3);
-const RechazoDePromesaNoManejado = new Efectos('Rechazo de promesa no manejado', 1, 'reducir resistencia del objetivo en 3', 'resiliencia', -2);
+const rechazoDePromesaNoManejado = new Efectos('Rechazo de promesa no manejado', 1, 'reducir resistencia del objetivo en 3', 'resiliencia', -2);
 const programacionEnPareja = new Efectos('Programacion en pareja', 3, 'aumentar el poder del objetivo en 2', 'poder', 2);
 
-console.table({redBelt, blackBelt})
-algoritmoDíficil.played(blackBelt);
-console.table({redBelt, blackBelt})
+algoritmoDíficil.played(redBelt);
+rechazoDePromesaNoManejado.played(redBelt);
+programacionEnPareja.played(redBelt);
 redBelt.attack(blackBelt);
-console.table({redBelt, blackBelt})
-programacionEnPareja.played(blackBelt)
+console.table({redBelt, blackBelt});
+
+// console.table({redBelt, blackBelt})
+// algoritmoDíficil.played(blackBelt);
+// console.table({redBelt, blackBelt})
+// redBelt.attack(blackBelt);
+// console.table({redBelt, blackBelt})
+// programacionEnPareja.played(blackBelt)
 // creo una propiedad se le asigna objetos[poder]
-console.table({redBelt, blackBelt, resDeYellow: blackBelt["poder"]})
-
-//Rep
-//Rep
-
-
+//console.table({redBelt, blackBelt, resDeYellow: blackBelt["poder"]()})
