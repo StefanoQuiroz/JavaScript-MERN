5e73d1d34033cd4f226a0355731660240c213ace
Stefano <piero.quiroz93@gmail.com>
2021-04-18 11:07:56 -0500

    Game Cards

--- gameCards/gameCards.js
+++ gameCards/gameCards.js
@@ -27,29 +27,26 @@ class Efectos extends Carta{
     }
     played(target){
         if(!(target instanceof Unidad)) throw new Error('Solo puedes atacar a otra unidad');
-
-        if(this.stat === 'Resiliencia'){
-            target.resiliencia+=this.magnitud;
-        }
-        if (this.stat === 'Poder'){
-            target.poder += this.magnitud;
-        }
+        target[this.stat] += this.magnitud;
     }
+      
+        
 }
 
-const redBelt = new Unidad("Ninja Cinturon Rojo", 3, 3, 4);
-const blackBelt = new Unidad ("Ninja Cinturon Negro",4,5,4);
+const redBelt = new Unidad("Ninja Cinturon Rojo", 2, 3, 3);
+const blackBelt = new Unidad ("Ninja Cinturon Negro",2,3,3);
 
-const algoritmoDíficil = new Efectos('Algoritmo Difícil', 2, 'aumentar resistencia del objetivo en 3', 'Resiliencia', 3);
-const RechazoDePromesaNoManejado = new Efectos('Rechazo de promesa no manejado', 1, 'reducir resistencia del objetivo en 3', 'Resiliencia', -2);
-const programacionEnPareja = new Efectos('Programacion en pareja', 3, 'aumentar el poder del objetivo en 2', 'Poder', 2);
-console.table({redBelt, blackBelt});
-algoritmoDíficil.played(redBelt);
-console.table({redBelt, blackBelt});
-programacionEnPareja.played(redBelt);
-console.table({redBelt, blackBelt});
-//redBelt.attack(blackBelt);
-//console.table({redBelt, blackBelt});
+const algoritmoDíficil = new Efectos('Algoritmo Difícil', 2, 'aumentar resistencia del objetivo en 3', 'resiliencia', 3);
+const RechazoDePromesaNoManejado = new Efectos('Rechazo de promesa no manejado', 1, 'reducir resistencia del objetivo en 3', 'resiliencia', -2);
+const programacionEnPareja = new Efectos('Programacion en pareja', 3, 'aumentar el poder del objetivo en 2', 'poder', 2);
+
+console.table({redBelt, blackBelt})
+algoritmoDíficil.played(blackBelt);
+console.table({redBelt, blackBelt})
+redBelt.attack(blackBelt);
+console.table({redBelt, blackBelt})
+programacionEnPareja.played(blackBelt)
+console.table({redBelt, blackBelt})
 
 
 
